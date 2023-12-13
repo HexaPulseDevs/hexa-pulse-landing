a3d26bc83ea03c885d2e7493e089095c31761d0d
@RupertoTacuche <murcio2490@gmail.com>
2023-12-12 20:05:57 -0600

    mi aportacion

--- src/components/introduccion.jsx
+++ src/components/introduccion.jsx
@@ -1,12 +1,8 @@
+import React from 'react'
 
-
-
-function App(): JSX.Element {
+const introduccion = () => {
   return (
-    <>
-      <div>
-        <h1>Hexa Pulse</h1>
-        <p>
+    <p>
         Bienvenido a Hexa-Pulse, tu socio confiable en el mundo de la consultoría para aplicaciones web. En un paisaje digital en constante evolución, cada línea de código cuenta, y cada experiencia del usuario forma la narrativa de tu marca.
 
 En Hexa-Pulse, entendemos la importancia de no solo crear aplicaciones web, sino de construir experiencias cautivadoras y funcionales. Nos enorgullece ofrecer servicios de consultoría de primera categoría, diseñados para impulsar tu presencia en línea al siguiente nivel.
@@ -15,9 +11,7 @@ Nuestro equipo de expertos en desarrollo web y consultores estratégicos están 
 
 Descubre cómo podemos hacer que tus sueños digitales se conviertan en realidad. ¡Bienvenido a la excelencia en consultoría para aplicaciones web con Hexa-Pulse
     </p>
-      </div>
-    </>
-  );
+  )
 }
 
-export default App;
+export default introduccion
